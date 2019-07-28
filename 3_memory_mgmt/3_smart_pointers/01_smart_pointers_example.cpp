b09070f31e86773808896d1233df2110df73a779
Deep Doshi <deepdoshi@live.in>
2019-07-27 23:59:04 -0700

    chore: rephrase comments

--- 3_memory_mgmt/3_smart_pointers/01_smart_pointers_example.cpp
+++ 3_memory_mgmt/3_smart_pointers/01_smart_pointers_example.cpp
@@ -4,11 +4,11 @@
 
 class testUnique {
   // No need to explicitly disable value-copying with this approach. 
-  // Now, here we have unique_ptr preventing this behavior
+  // Now, here unique_ptr prevents this behavior
    std::unique_ptr<int> ptr;
 public:
    /*
-   without unique_ptr we would do something like:
+   without unique_ptr it can be done by something like:
         testUnique(const testUnique&);
         testUnique& operator=(const testUnique&);
    */
@@ -21,12 +21,12 @@ public:
 };
 int main() {
 
-    // here we can see that we are trying to make container object for shared pointer
-    // we are using share pointers for our unique Class
-    // using this we can insure that we have multiple pointers point to totally unique object in application
+    // here a container object for shared pointer is made
+    // share pointers is used for own unique Class
+    // using this it can be insured that multiple pointers point to totally unique object in application
     // which doesn't have any copies
     std::vector<std::shared_ptr<testUnique> > container;
-    // we are adding couple objects to our object container (vector):
+    // adding couple objects to object container (vector):
     container.push_back(std::make_shared<testUnique> (testUnique(2)));
     container.push_back(std::make_shared<testUnique> (testUnique(52)));
     // Printing results:
