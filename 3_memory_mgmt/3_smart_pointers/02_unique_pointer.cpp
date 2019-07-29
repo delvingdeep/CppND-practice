e9eccf0065844a0854bb5e0620991c51422eaecf
Deep Doshi <deepdoshi@live.in>
2019-07-28 23:59:00 -0700

    add: unique pointer exercise

---
+++ 3_memory_mgmt/3_smart_pointers/02_unique_pointer.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <memory>
+
+class A
+{
+    public:
+        void print() { std::cout << "Printing from class A" << "\n"; }    
+};
+
+int main() {
+    std::unique_ptr<A> ptr (new A);
+    ptr->print();
+
+    // get memory address
+    std::cout << "ptr : " << ptr.get() << "\n";
+
+    // move ptr to another pointer
+    std::unique_ptr<A>  new_ptr = std::move(ptr);
+    new_ptr->print();
+    std::cout << "ptr : " << ptr.get() << "\n";
+    std::cout << "new_ptr : " << new_ptr.get() << "\n";
+
+    // move new_ptr to another pointer
+    std::unique_ptr<A>  latest_ptr = std::move(new_ptr);
+    latest_ptr->print();
+    std::cout << "ptr : " << ptr.get() << "\n";
+    std::cout << "new_ptr : " << new_ptr.get() << "\n";
+    std::cout << "latest_ptr : " << latest_ptr.get() << "\n";
+
+    return 0;
+}
