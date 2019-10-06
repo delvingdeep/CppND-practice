3ed05eaca34243d7d479939809194a0829eadec7
Deep Doshi <deepdoshi@live.in>
2019-10-05 23:59:52 -0700

    add: data races

---
+++ 4_concurrency/2_passing_data_between_threads/06_data_races.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <thread>
+#include <future>
+
+class Vehicle
+{
+  public:
+    // default constructor
+    Vehicle() : _id(0) {
+        std::cout << "Vehicle #" << id << " Default constructor called" << std::endl;
+    }
+
+    // initializing constructor
+    Vehicle(int id) : _id(id) {
+        std::cout << "Vehicle #" << id << " Initialzing constructor called" << std::endl; 
+    }
+
+    // setter and getter
+    void setID(int id) { _id = id; }
+    int getID() { return _id; }
+
+  private:
+    int _id;
+};
+
+int main() {
+    // create instances of class Vehicle
+    Vehicle v0;     // default constructor
+    Vehicle v1(1);  // initializing constructor
+
+    // read and write name in different threads
+    std::future<void> ftr = std::async([](Vehicle v) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(500)); // simulate work
+        v.setID(2);
+    }, v0);
+
+    v0.setID(3);
+
+    ftr.wait();
+    std::cout << "Vehicle #" << v0.getID() << std::endl;
+
+    return 0;
+}
